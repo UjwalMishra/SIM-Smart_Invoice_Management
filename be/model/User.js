fa74a2bb5a1a1481e0260d1dae0cbeae16f20fe2
Ujwal <ujwalcse01@gmail.com>
2025-07-19 17:59:37 +0530

    SIM/BE - created user model

---
+++ be/model/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    google_id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+    },
+    access_token: {
+      type: String,
+      required: true,
+    },
+    refresh_token: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
